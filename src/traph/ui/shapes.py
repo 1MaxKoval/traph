3be1cb93d1bfcb41c29b3a85f1c966cdd0e30d3a
max_k <koval6813@gmail.com>
2022-09-26 15:56:50 +0200

    Finished MessageBox
    - Constructor is retarded?

--- src/traph/ui/shapes.py
+++ src/traph/ui/shapes.py
@@ -1,3 +1,5 @@
+from curses.textpad import rectangle
+from re import T
 from typing import Tuple, List, Callable
 from .ui import render, remove, BACKGROUND_C, TERMINAL
 from bresenham import bresenham
@@ -64,7 +66,8 @@ class Shape:
         text_b = text_b if text_b is not None else {}
         for point in self.points:
             if point in color_map and point in text:
-                raise Exception('A point cannot be a text point and a color tile at the same time!')
+                # Handle the condition of a point being contained in text and in color tiles at the same time
+                del color_map[point]
             if point not in color_map:
                 color_map[point] = fill
         self.point_layers = render(
@@ -95,25 +98,23 @@ class Line(Shape):
 class MessageBox(Shape):
 
     def __init__(self, position: str, message: str):
-        pass
-        super().__init__()
-
+        if len(message) == 0:
+            raise Exception('Message cannot be an empty string!')
         self.m = message
         max_width = 0
-        height = 0
+        height = 1
         line_width = 0
-        for char in message:
+        for char in self.m:
             if char == '\n':
                 height += 1
-                max_width = max(max_width, line_width)
-                line_width = 0 
+                line_width = 0
             else:
                 line_width += 1
-            line_width += 1
+            max_width = max(max_width, line_width)
 
         self.height, self.width = height, max_width
         if position != 'c' and position != 'tl' and position != 'tr':
-            raise Exception()
+            raise Exception(f'Uknown position \'{position}\' for MessageBox')
         elif position == 'tl':
             top_left = (TERMINAL.width // 25, TERMINAL.height // 15)
         elif position == 'tr':
@@ -122,12 +123,31 @@ class MessageBox(Shape):
         elif position == 'c':
             top_left = (TERMINAL.width // 2 - self.width // 2, TERMINAL.height // 2 - self.height // 2)
 
-        # Add 2 to have a 1 pixel border on each side
         self.top_left = top_left
-        self.points = rectangle_points(top_left, self.width + 2, self.height + 2)
+        self.text_map = {}
+        dx, dy = 0, 0
+        ox, oy = top_left[0] + 1, top_left[1] + 1
+        for char in self.m:
+            if char == '\n':
+                dy += 1
+                dx = 0
+                continue
+            if dx > self.width:
+                dx = 0
+                dy += 1
+            self.text_map[(ox + dx, oy + dy)] = char
+            dx += 1
+        super().__init__(rectangle_points, top_left, self.width + 2, self.height + 2)
     
-    def draw(box_color, text_foreground = 'black', text_background = 'white'):
-        pass
+    def draw(self, fill=TERMINAL.white, t_b='black', t_f='white'):
+        text_background = {point: t_b for point in self.text_map}
+        text_foreground = {point: t_f for point in self.text_map}
+        super().draw(
+            fill=fill, 
+            text=self.text_map, 
+            text_f=text_foreground,
+            text_b=text_background
+        )
 
 class Bar(Shape):
     pass
